47b8267f446e5ec3f2a21937eaa4ff0584e1da1c
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2022-06-18 21:03:26 +1000

    Delete about.svelte

--- src/routes/about.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>About</title>
-</svelte:head>
-
-<h1>About</h1>
-
-<p>Basil Papoutsidis is a Melbourne-based artist working predominantly in sculpture. Taking inspiration from automotive finishes and custom car culture, Basil combines the use of steel with high finish paint. Basil's work has been exhibited in galleries both nationally and internationally including James Makin Gallery, Bus Projects, Anna Pappas Gallery as well as at the 2014 International Mokuhanga Conference held in Tokyo, Japan. His work is held in numerous public and private collections including the Epworth Hospital.</p>
