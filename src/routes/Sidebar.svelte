b2422225445514eabc900e24a3f6a9dd5c06690a
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2022-06-22 23:09:24 +1000

    Update Sidebar.svelte

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -1,18 +1,30 @@
 <script>
 	import { INDEX, stations } from './Stations.js'
+	import Plus from './Plus.svelte';
+	import Minus from './Minus.svelte';
 	
 	let clicked;	
 	const unsubscribe = INDEX.subscribe(value => clicked = value);
 </script>
 
 <aside>
+	<Minus/>
 	
 	<!-- Want to include slideout or something here -->
+	<a id="info">?</a>
+	<!-- Want to include slideout or something here -->
 	<h1>{stations[clicked].name}</h1>
-	
+		<!-- needs an event dispatcher to work? -->
+	<!-- Maybe change to MUTE -->
+	<!-- Maybe change to BIND -->
+	<Plus/> 
 </aside>
 
 <style>
+	#info {
+		color: inherit;
+	}
+	
 	aside {
 		background: white;
 		border-left: 1px solid;
