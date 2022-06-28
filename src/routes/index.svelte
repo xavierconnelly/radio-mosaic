8c2948780be6cfc0ab47044120d75d13be622156
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2022-06-28 18:24:23 +1000

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,34 +1,75 @@
 <script>
 	import Clock from './Clock.svelte';
-	import Volume from './Volume.svelte';
-
+	import Plus from './Plus.svelte';
+	import Minus from './Minus.svelte';
+	
 	let y;
 		import { INDEX, stations } from './Stations.js'
-		import Sidebar from './Sidebar.svelte';
-
+		import Header from './Header.svelte';
+	let view = 'closed';
 	let clicked;	
 	const unsubscribe = INDEX.subscribe(value => clicked = value);
 </script>
 
 <svelte:window bind:scrollY={y}/>
+
+
 <div id="filler"></div>
 
 <div id="gradients">
-  <div class="one" style="background: #{stations[clicked].hex}"></div>
-  <div class="two"></div>
-  <div class="three"></div>
-  <div class="four"></div>
+	
+		<div class="image-wrap">
+		<div class="image"
+				 style="background-image: url({stations[clicked].image})">
+		</div>
+	</div>
+<!--
+  <div class="one"
+						style="background:
+									rgb{stations[clicked].one};
+									transition: 2s;">
+	</div>
+	
+	<div class="two" 
+		 style="background: 
+								 radial-gradient(
+								 at -10% 30%, 
+								 rgb{stations[clicked].two} 0%,
+								 rgba(255, 255, 255, 0) 50%);">
+	</div>
+	
+	  <div class="three" 
+			 style="background: 
+							radial-gradient(
+							at 20% 90%,
+							rgb{stations[clicked].three} 0%,
+							rgba(255, 255, 255, 0) 40%);
+							transition: 2s;">
+	</div>
+	
+  <div class="four"
+			 style="background: 
+							radial-gradient(
+							at 65% 150%,
+							rgba{stations[clicked].four} 0%,
+							rgba(255, 255, 255, 0) 60%);
+							transition: 2s;">
+	</div>
+	-->
 </div>
+<aside class="left">	<Minus/></aside>
+<aside class="right">	<Plus/></aside>
 
-
-
+	<Header />
 <div id="earth">
 	<div class="clock"
-			 style="transform: rotate({y}deg)">
+			 style="transform: rotate({y}deg);
+							   translate(-50%, -50%)">
 	{#each stations as station, i}
 			<Clock 
 					i={i}		
 					hex={station.hex}
+					one={station.one}
 					name={station.name}
 					utc={station.utc}
 					class={station.class}
@@ -37,16 +78,16 @@
 	</div>
 </div>
 
-<Volume />
-<Sidebar />
-
-
 <style>
 	:global(body) {
 			margin: 0;
 			padding: 0;
 			color: #F4F2EA;
+		background: #453939;
 	}
+* {
+  box-sizing: border-box;
+}
 	#filler{
 		height: 1000vh;
 		width: 50vw;
@@ -54,73 +95,80 @@
 	}
 	
 	#earth{
-		width: 500px;
-		height: 500px;
+    width: 225px;
+    height: 225px;
     position: fixed;
-    left: 50vw;
-    top: 50vh;
+    left: 50%;
+    bottom: 0;
     border-radius: 50%;
     z-index: 2;
-    transform: translate(-50%, -50%);
-}
+    transform: translate(-50%, -50%)
+	}
 	
 .clock {
-		height: 550px; 
-		width: 550px;  /*border: px solid pink;*/
-		border-radius: 50%; 
-		position: absolute;
-	 /* top: ;
-		/*transform: translate(-50%, -50%);*/
+		background: #453939;
+    height:225px;
+    width: 225px;
+    border-radius: 50%;
+    position: absolute;
+    bottom: 0;
 }
 
+	
+	
 #gradients {
-  height: 100vh;
-  width: 100vw;
-    position: fixed;
-    top: 0;
-    left: 0;
+  height: 900px;
+  width: 900px;
+  position: fixed;
+  bottom: -900px;
+  left: 50%;
+	transform: translate(-50%, -50%);
+  border-radius: 50%;
+  overflow: hidden;
 }
 
 #gradients div {
   height: 100%;
   width: 100%;
   position: absolute;
-  /*mix-blend-mode: multiply;*/
 }
 
-.one {
-  background: rgb(63, 94, 251);
-  background: radial-gradient(
-    at 60% 80%,
-    rgba(63, 94, 251, 1) 50%,
-    rgba(16, 107, 214, 0) 90%
-  );
-}
+.image-wrap {
+		background: #453939;
+	}
+	
+.image {
+	background-size: cover;
+	background-position: center;
+	height: 45vh;
+	filter: grayscale(100%);
+	color: inherit;
+		mix-blend-mode: screen;
+		transition: 1s;
+	}
+	/*
+	.one, .two, .three, .four {
+			transition: 1s;
+	}*/
 
-.two {
-  background: rgb(3, 113, 60);
-  background: radial-gradient(
-    at 15% 0%,
-    rgba(3, 113, 60, 1) 1%,
-    rgba(3, 113, 60, 0) 70%
-  );
-}
+aside {
+		position: fixed;
+		top: 0;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	
+	}
+	
+	.left {
+		left: 0;
+	}
+	
+	.right {
+		right: 0;
+	}
 
-.three {
-  background: rgb(208, 61, 2);
-  background: radial-gradient(
-    at 70% 30%,
-    rgba(208, 61, 2, 1) 40%,
-    rgba(208, 61, 2, 0) 80%
-  );
-}
 
-.four {
-  background: radial-gradient(
-    at 0% 80%,
-    rgba(3, 113, 60, 1) 10%,
-    rgba(3, 113, 60, 0) 90%
-  );
-}
 	
 </style>
