4254b337859cbb3e7a5fb5f8c6c438a1bf3bed23
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2025-12-03 07:45:53 +1100

    more sliding

--- src/lib/audio.svelte
+++ src/lib/audio.svelte
@@ -36,74 +36,63 @@
 	>
 </audio> -->
 
-<div>
-
-	<button onclick={() => paused = !paused}>
-		{paused ? "PLAY" : "STOP"}
-	</button>
-	<!-- output of the number-->
-	{Math.floor(volume * 100)} 
-</div>
+<button onclick={() => paused = !paused}>
+	{paused ? "PLAY" : "STOP"}
+</button>
+<!-- output of the number
+{Math.floor(volume * 100)} -->
 	
-<input type="range" 
-			 bind:value={volume}
-			 max="1"
-			 step="0.01"/>
+<input 	bind:value={volume}
+		type="range" 
+		max="1"
+		step="0.01"/>
 
 <style>
-	* {
-		margin: 0;
-	}
-	
-	div{
-		position: fixed;
-		left: 100px;
-		top: 100px;
-	}
 	
 /*Chrome*/
 @media screen and (-webkit-min-device-pixel-ratio:0) {
     input[type='range'] {
       overflow: hidden;
-      width: 100vh;
+      width: calc(100vh - 44px);
 			
       -webkit-appearance: none;
-      background-color: white;
-			transform: rotate(270deg) translate(calc(-50% + 15px), calc(-50vh + 50%));
-			position: fixed;
-			top: 0;
-			left: 0;
+      background-color: var(--yin);
+		/* transform: rotate(270deg) translate(calc(-50% + 15px), calc(-50vh + 50%)); */
+		transform: rotate(270deg);
+		position: fixed;
+		top: 44px;
+		right: 0;
     }
     
     input[type='range']::-webkit-slider-runnable-track {
       height: 30px;
       -webkit-appearance: none;
-      color: #13bba4;
+      color: red;
       margin-top: -1px;
     }
     
     input[type='range']::-webkit-slider-thumb {
       width: 30px;
       -webkit-appearance: none;
-      height: 30px;
+      height: 10px;
       cursor: cursor;
-      background: #453939;
-      box-shadow: -1000px 0 0 1000px #453939;
+      background: var(--yang);
+      box-shadow: -1000px 0 0 1000px var(--yang);
     }
 
 }
 /** FF*/
 input[type="range"]::-moz-range-progress {
-  background-color: white; 
+      background-color: var(--yin);
 }
 input[type="range"]::-moz-range-track {  
-  background-color: #453939;
+      background: var(--yang);
 }
 /* IE*/
 input[type="range"]::-ms-fill-lower {
-  background-color: white; 
+      background-color: var(--yin);
 }
 input[type="range"]::-ms-fill-upper {  
-  background-color: #453939;
+      background: var(--yang);
 }
 </style>
